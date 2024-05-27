e00e6c6ae4cfe4ecc8a7a6971229b30318023490
Himawan Prayogo <118036775+HimawanPrayogo@users.noreply.github.com>
2024-05-27 15:57:43 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php
+    // kode anda
+    require 'koneksi.php';
+
+    $sql = "DELETE FROM stockbarang WHERE idBarang='".$_GET['id']."'";
+    
+    $result = mysqli_query($conn, $sqql);
+    
+    if($result){
+        echo "Berhasil DELETE DATA";
+    }else{
+        echo "Gagal DELETE DATA";
+    }
+    $conn->close();
+
+    header("location:index.php") //untuk langsung ketika di klik balik ke halaman header()
+?>
+
+
+
+
+<!-- <a href="index.php">KEMBALI</a> -->

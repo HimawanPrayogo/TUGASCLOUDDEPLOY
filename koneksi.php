7651ebaea108f11ae537d63476a148f851aac374
Himawan Prayogo <118036775+HimawanPrayogo@users.noreply.github.com>
2024-05-28 11:06:14 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,15 +1,12 @@
 <?php
-    // $conn = mysqli_connect("127.0.0.1","root","","progweb") or die ("Koneksi ke DB gagal");
-
 
     $servername = getenv('DB_HOST');
     $username = getenv('DB_USERNAME');
     $password = getenv('DB_PASSWORD');
     $db = "progweb"
 
-    // $conn = mysqli_connect($servername,$username,$password,$db) or die ("Koneksi ke DB gagal");
+    $conn = mysqli_connect($servername,$username,$password,$db) or die ("Koneksi ke DB gagal");
 
-    $conn = mysqli_connect("tugas-mariadb-1","root","123","progweb") or die ("Koneksi ke DB gagal");
 ?>
 
 

f7bf614de9661324f7d133e723bb1426e8334fa8
Himawan Prayogo <118036775+HimawanPrayogo@users.noreply.github.com>
2024-05-29 10:04:52 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,7 @@
     $servername = getenv('DB_HOST');
     $username = getenv('DB_USERNAME');
     $password = getenv('DB_PASSWORD');
-    $db = "progweb";
+    $db = getenv('DB_NAME');
 
     $conn = mysqli_connect($servername,$username,$password,$db) or die ("Koneksi ke DB gagal");
 
